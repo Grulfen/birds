7dc6ab2273f71dc0c16f8dc9c906e72cce7b5caf
Gustav Svensk <grulfen3@gmail.com>
2024-03-19 22:11:10 +0100

    Run black

--- birds/bird_fft.py
+++ birds/bird_fft.py
@@ -5,13 +5,13 @@ import numpy
 
 
 def wav_to_frames(wav_file: str) -> List[int]:
-    " open a .wav file and convert it to a list of samples "
+    "open a .wav file and convert it to a list of samples"
     wav = wave.open(wav_file, "rb")
     return numpy.frombuffer(wav.readframes(-1), numpy.int16)
 
 
 def absolute_fft(signal):
-    " Do a discrete fft on @signal and return the absolute value "
+    "Do a discrete fft on @signal and return the absolute value"
     return numpy.absolute(numpy.fft.rfft(signal))
 
 
