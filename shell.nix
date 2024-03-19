93a6babb47b4d53e27dacddbe3950819b1293966
Gustav Svensk <grulfen3@gmail.com>
2024-03-19 22:10:35 +0100

    Rewrite shell.nix to use python3.withPackages

--- shell.nix
+++ shell.nix
@@ -1,30 +1,33 @@
 { pkgs ? import <nixpkgs> {} }:
   pkgs.mkShell {
-    nativeBuildInputs = with pkgs.buildPackages; [
-        python310Packages.aiohttp
-        python310Packages.black
-        python310Packages.click
-        python310Packages.h5py
-        python310Packages.hypothesis
-        python310Packages.ipdb
-        python310Packages.ipython
-        python310Packages.isort
-        python310Packages.jupyter-core
-        python310Packages.keras
-        python310Packages.librosa
-        python310Packages.notebook
-        python310Packages.numpy
-        python310Packages.pylsp-mypy
-        python310Packages.pytest
-        python310Packages.python-lsp-ruff
-        python310Packages.python-lsp-server
-        python310Packages.scikit-learn
-        python310Packages.seaborn
-        python310Packages.soundfile
-        python310Packages.tensorflow
-        python310Packages.termcolor
-        python310Packages.tqdm
+    buildInputs = [
+      (pkgs.python3.withPackages (ps: with ps; [
+        aiohttp
+        black
+        click
+        h5py
+        hypothesis
+        ipdb
+        ipython
+        isort
+        jupyter-core
+        keras
+        librosa
+        notebook
+        numpy
+        pylsp-mypy
+        pytest
+        python-lsp-ruff
+        python-lsp-server
+        scikit-learn
+        seaborn
+        soundfile
+        tensorflow
+        termcolor
+        tqdm
+      ]))
     ];
+
   # Workaround for broken matplotlib package
   shellHook = ''
     export TK_LIBRARY="${pkgs.tk}/lib/${pkgs.tk.libPrefix}"
